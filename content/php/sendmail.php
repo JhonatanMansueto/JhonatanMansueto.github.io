5246ecff7d69744f1d24265cb444e9632334339c
Jhonatan Mansueto <jhonatanmansueto@gmail.com>
2024-10-07 21:35:20 -0300

    ClubeVida10 07/10/2024

--- content/php/sendmail.php
+++ content/php/sendmail.php
@@ -1,11 +1,11 @@
 <?php
-    $fromname = htmlspecialchars_decode($_POST["nome"]);
-    $frommail = htmlspecialchars_decode($_POST["email"]);
-    $phone = htmlspecialchars_decode($_POST["telefone"]);
-    $to = "contato@jhonatanmachado.com";
-    $subject = htmlspecialchars_decode($_POST["assunto"]);
+    $fromname = htmlspecialchars_decode($_POST["name"]);
+    $frommail = htmlspecialchars_decode($_POST["mail"]);
+    $phone = htmlspecialchars_decode($_POST["phone"]);
+    $to = "contato@projetovida10.com.br";
+    $subject = htmlspecialchars_decode($_POST["subject"]);
     $contact = $fromname . " " . $phone;
-    $message = $contact . "\n" . htmlspecialchars_decode($_POST["menssagem"]);
+    $message = $contact . "\n" . htmlspecialchars_decode($_POST["message"]);
     $headers = "From: " . $fromname . " <" . $frommail . ">";
 
     //echo $frommail . " " . $fromname . " " . $subject . " " . $headers;
@@ -16,7 +16,7 @@
             mail($to, $subject, $message, $headers);
         }
         if($i == 1){
-            header("Location: /email-enviado.html");
+            header("Location: /index.html#bannerContato");
             die();
         }
         $i = $i + 1;
